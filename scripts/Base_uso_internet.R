945b23ce231a52f0e38aa37f606ef4342c007013
nataliarodriguezfr <natalia.rodriguezfr@urosario.edu.co>
2025-08-17 17:42:37 -0500

    Add files via upload

--- scripts/Base_uso_internet.R
+++ scripts/Base_uso_internet.R
@@ -1,5 +1,4 @@
-library(gt)
-library(webshot2)
+library(writexl)
 library("haven")
 library(dplyr)
 library(readxl)
@@ -11,7 +10,7 @@ library(stringr)
 setwd("C:\\Users\\natal\\OneDrive\\Documentos")
 
 
-datos <- read_dta("TenderosFU03_Publica.dta")
+tenderos <- read_dta("TenderosFU03_Publica.dta")
 poblacion <- read_excel("TerriData_Dim2_Sub3.xlsx")
 
 poblacion <- poblacion[(poblacion$Año==2024),] #Filtramos por año
@@ -21,9 +20,9 @@ poblacion$`Dato Numérico` <- as.numeric(gsub(",", ".", gsub("\\.", "", poblacio
 
 
 #1. Tarea 1
-datos_municipio <- datos %>%
+datos_internet_municipio <- tenderos %>%
   group_by(Munic_Dept) %>%
-  summarise( Municipio =first(Municipio), uso_internet = mean(uso_internet, na.rm = TRUE)) 
+  summarise( Municipio =first(Municipio), uso_internet = (mean(uso_internet, na.rm = TRUE)*100))
 
 #. Tarea 2
 
@@ -44,10 +43,10 @@ tabla_actividades <- tibble(
   )
 )
 
-act <- datos %>%
+act_ancho <- tenderos %>%
   group_by(Municipio) %>%
   summarise(across(actG1:actG11, sum, na.rm = TRUE)) 
-act_long <- act %>%
+act_long <- act_ancho %>%
   pivot_longer(cols = starts_with("actG"),
                names_to = "actividad") %>%
   mutate(actividad = str_remove(actividad, "actG")) %>%
@@ -55,7 +54,7 @@ act_long <- act %>%
   left_join(tabla_actividades, by = "actividad")
 
 # Tarea 3
-actividad <- datos_municipio %>%
+actividad <- datos_internet_municipio %>%
   left_join(act_long, by = c("Municipio"))
 actividad <- actividad %>%
   select(Munic_Dept, Municipio, actG = actividad, actividad = nombre_actividad, internet = uso_internet )
@@ -63,7 +62,8 @@ actividad <- actividad %>%
 #Tarea 4
 poblacion_agg <- poblacion%>%
   mutate(codigo = as.numeric(`Código Entidad`)) %>%
-  group_by(codigo) %>%
+  mutate(Municipio = as.character(Entidad)) %>%
+  group_by(codigo, Municipio) %>%
   summarise(poblacion = sum(`Dato Numérico`))
 
 #Tarea 5
@@ -79,33 +79,76 @@ poblacion_agg <- poblacion_agg %>%
 base_final1 <- actividad %>%
   inner_join(poblacion_agg, by = c("Munic_Dept" = "codigo"))
 
-base_final1
+
+#Tarea 5.2
+arch1_opcion1 <- tenderos %>%
+  group_by(Municipio) %>%
+  summarise(total_tiendas = sum(actG1, na.rm = TRUE))  # actG1 es "Tienda"
+
+arch2_opcion1 <- tenderos %>%
+  filter(uso_internet == 1) %>%        # solo tiendas con internet
+  group_by(Municipio) %>%
+  summarise(tiendas_internet = sum(actG1, na.rm = TRUE)) %>%
+  rename(Municipio = Municipio)
+
+base_final_opcion1 <- inner_join(arch1_opcion1, arch2_opcion1, by = "Municipio")
+
+base_final_opcion1 <- base_final_opcion1 %>%
+  mutate(proporcion_internet = (tiendas_internet / total_tiendas)*100)
 
 
+#Tarea 5.3
+
+datos_clave <- tenderos %>% #Mantener variables clave
+  mutate(Munic_Dept = as.character(Munic_Dept)) %>%
+  select(Munic_Dept, Municipio, uso_internet, actG1:actG11)
+
+datos_long <- datos_clave %>% #Pasar actividades de ancho a largo
+  pivot_longer(cols = starts_with("actG"),
+               names_to = "actividad",
+               values_to = "valor") %>%
+  filter(valor == 1)   # Solo tiendas que tienen esa actividad
+
+tabla_actividades_opcion2 <- tibble( #Ponemos nombres a las actividades
+  actividad = paste0("actG", 1:11),
+  nombre_actividad = c(
+    "Tienda",
+    "Comida preparada",
+    "Peluquería y belleza",
+    "Ropa",
+    "Otras variedades",
+    "Papelería y comunicaciones",
+    "Vida nocturna",
+    "Productos bajo inventario",
+    "Salud",
+    "Servicios",
+    "Ferretería y afines"
+  )
+)
+datos_long <- datos_long %>% 
+  left_join(tabla_actividades_opcion2, by = "actividad")
 
 
-#--------------------Obtener imagenes--------------------#
+datos_long <- datos_long %>% #Agrupamos por municipios y actividad
+  group_by(Munic_Dept,Municipio, nombre_actividad) %>%
+  summarise(
+    total = n(),
+    con_internet = sum(uso_internet, na.rm = TRUE),
+    .groups = "drop"
+  ) %>%
+  mutate(prop_internet = (con_internet / total)*100)
 
-tabla_final1 <- gt(head(datos_municipio, 10)) #Guardamos tarea 1
-gtsave(tabla_final1, "C:/Users/natal/OneDrive - Universidad del rosario/Universidad/Segundo Semestre/Haciendo economía/Rstudio/Descargas/datos_municipio.png")
+poblacion_agg_opcion2 <- poblacion_agg %>% #Merge con poblacion
+  mutate(codigo = as.character(codigo)) %>%
+  select(codigo, poblacion)
+base_final_opcion2 <- datos_long %>%
+  left_join(poblacion_agg_opcion2, by = c("Munic_Dept" = "codigo"))
 
-tabla_final2 <- gt(head(tabla_actividades, 11)) #Guardamos tarea 2
-gtsave(tabla_final2, "C:/Users/natal/OneDrive - Universidad del rosario/Universidad/Segundo Semestre/Haciendo economía/Rstudio/Descargas/tabla_actividades.png")
+#--------------------Pasar a excel--------------------#
 
-tabla_final3 <- gt(head(act, 10))
-gtsave(tabla_final3, "C:/Users/natal/OneDrive - Universidad del rosario/Universidad/Segundo Semestre/Haciendo economía/Rstudio/Descargas/act.png")
+write_xlsx(base_final1, "C:/Users/natal/OneDrive - Universidad del rosario/Universidad/Segundo Semestre/Haciendo economía/Rstudio/Descargas/base_final1.xlsx")
 
-tabla_final4 <- gt(head(act_long, 110))
-gtsave(tabla_final4, "C:/Users/natal/OneDrive - Universidad del rosario/Universidad/Segundo Semestre/Haciendo economía/Rstudio/Descargas/act_long.png")
+write_xlsx(base_final_opcion1, "C:/Users/natal/OneDrive - Universidad del rosario/Universidad/Segundo Semestre/Haciendo economía/Rstudio/Descargas/base_final_opcion1.xlsx")
 
-tabla_final5 <- gt(head(actividad, 110)) #Guardamos tara 3
-gtsave(tabla_final5, "C:/Users/natal/OneDrive - Universidad del rosario/Universidad/Segundo Semestre/Haciendo economía/Rstudio/Descargas/actividad.png")
-
-tabla_final6 <- gt(head(poblacion_agg, 1135)) #Guardamos tarea 4
-gtsave(tabla_final6, "C:/Users/natal/OneDrive - Universidad del rosario/Universidad/Segundo Semestre/Haciendo economía/Rstudio/Descargas/poblacion_agg.png")
-
-tabla_final7 <- gt(head(base_final1, 110)) #Guardamos tarea 5
-gtsave(tabla_final7, "C:/Users/natal/OneDrive - Universidad del rosario/Universidad/Segundo Semestre/Haciendo economía/Rstudio/Descargas/base_final1.png")
-
-
+write_xlsx(base_final_opcion2, "C:/Users/natal/OneDrive - Universidad del rosario/Universidad/Segundo Semestre/Haciendo economía/Rstudio/Descargas/base_final_opcion2.xlsx")
 
